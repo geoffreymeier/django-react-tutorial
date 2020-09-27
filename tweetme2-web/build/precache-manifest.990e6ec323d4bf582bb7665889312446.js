73f77c86750bf873365afcb72f22daa367fbeb40
Geoffrey Meier <gmeier108@gmail.com>
2020-09-26 22:57:17 -0600

    integrated react with django

---
+++ tweetme2-web/build/precache-manifest.990e6ec323d4bf582bb7665889312446.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb31b4df2c639223d22ba96aadfafedb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1b5a2e141685941084b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "995d47db6af999cfe417",
+    "url": "/static/js/2.e01fd780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e01fd780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b5a2e141685941084b",
+    "url": "/static/js/main.1c60bab7.chunk.js"
+  },
+  {
+    "revision": "43cfd09573b07f1c8945",
+    "url": "/static/js/runtime-main.99d83fc2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lb-s^_=$=e-2vtv!)#c8iagbxnq*njakzu)%rgz#&mu=4zqqer'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 LOGIN_URL = "/login"
 
@@ -46,12 +46,14 @@ INSTALLED_APPS = [
     #internal
     'tweets',
     #third-party
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,6 +130,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static-root")
+
+CORS_ORIGIN_ALLOW_ALL = True #any website has access has access to my api
+CORS_URLS_REGEX = r'^/api/.*$'
 
 # REST_FRAMEWORK = {
 #     "DEFAULT_AUTHENTICATION_CLASSES": {
